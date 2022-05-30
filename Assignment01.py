43e162473fadbee6f7995632fb72a3868840c499
Tanmay Jadhav <87440930+TanmayAJadhav@users.noreply.github.com>
2022-05-31 00:45:23 +0530

    Set operations using python without using any inbuilt function

--- Assignment01.py
+++ Assignment01.py
@@ -1,4 +1,5 @@
-# In second year, computer engineering class, group A student's play cricket, group B students play badminton and group C students play football. Write a Python program using functions to compute following: -
+# In second year, computer engineering class, group A student's play cricket, group B students play badminton and group C students play football. Write a Python
+# program using functions to compute following: -
 # a) List of students who play both cricket and badminton
 # b) List of students who play either cricket or badminton but not both
 # c) Number of students who play neither cricket nor badminton
